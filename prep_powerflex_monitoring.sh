2df381c940993810a4296aa315c14b2310a4ac27
Brian Dean <brian.dean2@dell.com>
2026-03-11 16:38:41 -0400

    copyright owner edit

--- prep_powerflex_monitoring.sh
+++ prep_powerflex_monitoring.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2026 Dell Technologies
+# Copyright 2026 Dell, Inc.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
